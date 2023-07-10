83c6c9078158eb7fb59335aa68c3f817afe7e42c
Mohammad Aman <mohammadamaan1105@gmail.com>
2023-07-10 12:59:58 +0545

    commit changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,39 +1,30 @@
-import React from 'react';
-import '../styles/contact.css';
+import "../styles/contact.css";
 
-const Contact = () => {
-    return (
-        <>
-            <div className="container">
-
-
-                <h1>Contact us</h1>
-                <div className="contact-box">
-                    <form action="/">
-                        <div className="nameBox">
-                            <label htmlFor="Name">Name</label>
-                            <div>
-                                <input placeholder='First Name' type="text" name="name" id="name" />
-                                <input placeholder='Last Name' type="text" name="name" id="name" />
-                            </div>
-                        </div>
-                        <div className="emailBox">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" />
-                        </div>
-                        <div className="messageBox">
-                            <label htmlFor="message">Comment or Message</label>
-                            <textarea placeholder='Enter something about yourself here...' name="message" id="commentOrMessage" cols="60" rows="10"></textarea>
-                        </div>
-                        <div className='btn'>
-                            <button>Submit</button>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </>
-    )
+function Contact() {
+  return (
+    <div className="contactMainContainer">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <h1>Contact Us</h1>
+        <div className="nameLabelInputBox">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder="Enter your name here..." />
+        </div>
+        <div className="emailLabelInputBox">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your Email here..."
+          />
+        </div>
+        <div className="textareaDiv">
+            <h3>Comment</h3>
+            <textarea cols="20" rows="10" placeholder="Leave messages here..."></textarea>
+        </div>
+        <button className="formSubmitBtn">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
